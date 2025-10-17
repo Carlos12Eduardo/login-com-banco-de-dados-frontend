5a574160c9a4cc6dff3c61e615e1b0f052fc1fd5
Carlos Silva <carlos110eduardo@gmail.com>
2025-10-17 16:00:06 -0300

    adicionado a função autenticar usuario

--- src/form/form.jsx
+++ src/form/form.jsx
@@ -1,11 +1,42 @@
+import { useEffect, useRef, useState } from "react";
+
 export const Form = () => {
+    const loginUsuario = useRef("");
+    const senhaUsuario = useRef("");
+    const [clicou, setClicou] = useState(0);
+    const hundleSumbit = () => {
+        setClicou(clicou + 1);
+        console.log(clicou);
+        console.log(loginUsuario.current.value);
+        console.log(senhaUsuario.current.value);
+    }
+    useEffect(() =>{
+        const autenticarUsuario = async () => {
+            const consulta = await fetch("http://localhost:3000/autenticar", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    "login": loginUsuario.current.value,
+                    "senha": senhaUsuario.current.value
+                })
+            });
+            const resposta = await consulta.json();
+            // console.log(resposta);
+            console.log(consulta);
+            // if(consulta.status == 200) 
+            alert(resposta.mensagem);
+        };
+        autenticarUsuario();
+    },[clicou]);
     return (
         <>
             <label htmlFor="login-usuario">Login</label>
-            <input type="text" name="login-usuario" id="login-usuario"/>
+            <input type="text" name="login-usuario" id="login-usuario" ref={loginUsuario}/>
             <label htmlFor="senha-usuario">Senha</label>
-            <input type="password" name="senha-usuario" id="senha-usuario" />
-            <button type="submit">Entrar</button>
+            <input type="password" name="senha-usuario" id="senha-usuario" ref={senhaUsuario}/>
+            <button type="submit" onClick={hundleSumbit}>Entrar</button>
         </>
     );
 }
